625a7bcc9416375195b1b3f899248059093ae968
JonSchram <7951615+JonSchram@users.noreply.github.com>
2023-12-17 16:30:56 -0500

    Enable Maven in project, add Apache Commons CLI.
    Uses commons CLI as a dependency, adds some initial command line
    arguments that will most likely be needed. Still figuring out the best
    way to define and process arguments.

---
+++ src/main/java/com/jonathanschram/vtt/ConverterCli.java
@@ -0,0 +1,66 @@
+package com.jonathanschram.vtt;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class ConverterCli {
+
+	public static void main(String[] args) throws ParseException {
+		CommandLine commandArgs = parseCommandLineArgs(args);
+		
+		if (!commandArgs.hasOption("help") && !commandArgs.hasOption("version")) {
+			// Neither "help" or "version" is set, continue.
+			
+		}
+	}
+
+	private static CommandLine parseCommandLineArgs(String[] args) throws ParseException {
+		/*
+		 * According to this (https://stackoverflow.com/a/10798771) answer by Forage
+		 * (https://stackoverflow.com/users/143224/forage) on StackOverflow, Apache
+		 * Commons CLI doesn't allow setting some arguments required without also
+		 * allowing a "help" option to be used without those required arguments.
+		 * 
+		 * With the number of arguments that will likely be required to configure the
+		 * conversion, I think it would be easier to manually validate the arguments.
+		 * 
+		 */
+
+		Options options = new Options();
+
+		// Rendering.
+		options.addOption(Option.builder("x").longOpt("xresolution").type(Integer.class)
+				.desc("The x-resolution of the screen that ASS subtitles will be rendered on. Required.").build());
+		options.addOption(Option.builder("y").longOpt("yresolution").type(Integer.class)
+				.desc("The y-resolution of the screen that ASS subtitles will be rendered on. Required").build());
+		options.addOption(Option.builder("m").longOpt("mediumfontsize").hasArg().type(Integer.class)
+				.desc("Size of medium font in points. Other font sizes will be calculated from this value.").build());
+
+		// Features unique to ASS, configure workaround.
+		options.addOption(Option.builder().longOpt("verticalbehavior").hasArg().desc(
+				"How to handle vertical styles in VTT file. \"rotate\" will rotate the entire subtitle (default). \"break\" will insert line breaks after each character.")
+				.build());
+
+		// Informational.
+		options.addOption(Option.builder("v").longOpt("version").desc("Print version information").build());
+		options.addOption(Option.builder("h").longOpt("help").desc("Print this message").build());
+
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse(options, args);
+		
+		if (cmd.hasOption("help")) {
+			// Help formatter must have access to options.
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("vttconverter", options, true);
+		}
+		
+		return cmd;
+
+	}
+
+}
