1fd2113054ed9cec3e046e3e36b096a4b3ce0212
JonSchram <7951615+JonSchram@users.noreply.github.com>
2024-02-02 18:17:14 -0500

    Refactors NodeStyle to use CSS properties instead of resolved values.
    This will be used during style calculation, so it needs to have the
    intermediate CSS value.

--- src/main/java/com/jonathanschram/vttconverter/lib/vtt/css/NodeStyle.java
+++ src/main/java/com/jonathanschram/vttconverter/lib/vtt/css/NodeStyle.java
@@ -1,91 +1,91 @@
 package com.jonathanschram.vttconverter.lib.vtt.css;
 
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.Opacity;
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.TextCombineUpright;
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.TextShadow;
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.Visibility;
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.WhiteSpace;
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.background.Background;
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.decoration.TextDecoration;
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.font.Font;
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.outline.Outline;
-import com.jonathanschram.vttconverter.lib.vtt.css.properties.ruby.RubyPosition;
-import com.jonathanschram.vttconverter.lib.vtt.css.types.AbsoluteColor;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.CssColor;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.CssOpacity;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.CssRubyPosition;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.CssTextCombineUpright;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.CssTextShadow;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.CssVisibility;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.CssWhitespace;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.background.CssBackground;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.decoration.CssTextDecoration;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.font.CssFont;
+import com.jonathanschram.vttconverter.lib.vtt.css.cascade.outline.CssOutline;
 
 /***
  * The set of CSS properties that can apply to a cue node.
  */
 public class NodeStyle {
-    private final AbsoluteColor color;
-    private final Opacity opacity;
-    private final Visibility visibility;
-    private final TextDecoration textDecoration;
-    private final TextShadow textShadow;
-    private final Background background;
-    private final Outline outline;
-    private final Font font;
-    private final WhiteSpace whiteSpace;
-    private final TextCombineUpright textCombineUpright;
-    private final RubyPosition rubyPosition;
+    private final CssBackground background;
+    private final CssColor color;
+    private final CssFont font;
+    private final CssOpacity opacity;
+    private final CssOutline outline;
+    private final CssRubyPosition rubyPosition;
+    private final CssTextCombineUpright textCombineUpright;
+    private final CssTextDecoration textDecoration;
+    private final CssTextShadow textShadow;
+    private final CssVisibility visibility;
+    private final CssWhitespace whiteSpace;
 
-    public NodeStyle(AbsoluteColor color, Opacity opacity, Visibility visibility, TextDecoration textDecoration,
-            TextShadow textShadow, Background background, Outline outline, Font font, WhiteSpace whiteSpace,
-            TextCombineUpright textCombineUpright, RubyPosition rubyPosition) {
-        super();
-        this.color = color;
-        this.opacity = opacity;
-        this.visibility = visibility;
-        this.textDecoration = textDecoration;
-        this.textShadow = textShadow;
-        this.background = background;
-        this.outline = outline;
-        this.font = font;
-        this.whiteSpace = whiteSpace;
-        this.textCombineUpright = textCombineUpright;
-        this.rubyPosition = rubyPosition;
+    /***
+     * Constructor for NodeStyle that has all values unset.
+     */
+    public NodeStyle() {
+        background = new CssBackground();
+        color = new CssColor();
+        font = new CssFont();
+        opacity = new CssOpacity();
+        outline = new CssOutline();
+        rubyPosition = new CssRubyPosition();
+        textCombineUpright = new CssTextCombineUpright();
+        textDecoration = new CssTextDecoration();
+        textShadow = new CssTextShadow();
+        visibility = new CssVisibility();
+        whiteSpace = new CssWhitespace();
     }
 
-    public AbsoluteColor getColor() {
-        return color;
-    }
-
-    public Opacity getOpacity() {
-        return opacity;
-    }
-
-    public Visibility getVisibility() {
-        return visibility;
-    }
-
-    public TextDecoration getTextDecoration() {
-        return textDecoration;
-    }
-
-    public TextShadow getTextShadow() {
-        return textShadow;
-    }
-
-    public Background getBackground() {
+    public CssBackground getBackground() {
         return background;
     }
 
-    public Outline getOutline() {
-        return outline;
+    public CssColor getColor() {
+        return color;
     }
 
-    public Font getFont() {
+    public CssFont getFont() {
         return font;
     }
 
-    public WhiteSpace getWhiteSpace() {
-        return whiteSpace;
+    public CssOpacity getOpacity() {
+        return opacity;
     }
 
-    public TextCombineUpright getTextCombineUpright() {
+    public CssOutline getOutline() {
+        return outline;
+    }
+
+    public CssRubyPosition getRubyPosition() {
+        return rubyPosition;
+    }
+
+    public CssTextCombineUpright getTextCombineUpright() {
         return textCombineUpright;
     }
 
-    public RubyPosition getRubyPosition() {
-        return rubyPosition;
+    public CssTextDecoration getTextDecoration() {
+        return textDecoration;
+    }
+
+    public CssTextShadow getTextShadow() {
+        return textShadow;
+    }
+
+    public CssVisibility getVisibility() {
+        return visibility;
+    }
+
+    public CssWhitespace getWhiteSpace() {
+        return whiteSpace;
     }
 }
--- src/main/java/com/jonathanschram/vttconverter/lib/vtt/css/cascade/CssOpacity.java
+++ src/main/java/com/jonathanschram/vttconverter/lib/vtt/css/cascade/CssOpacity.java
@@ -4,9 +4,12 @@ import com.jonathanschram.vttconverter.lib.vtt.css.properties.Opacity;
 
 public class CssOpacity extends CascadingStyle<Opacity> {
 
+    public CssOpacity() {
+    }
+
     public CssOpacity(double value) {
     }
-    
+
     @Override
     public Opacity getInitialValue() {
         return new Opacity(1);
